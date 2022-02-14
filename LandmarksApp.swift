0f32d19abdd08a3e042746f1319fbd6b0d1fd547
commitcomplete <oneblue14@gmail.com>
2022-02-14 13:27:51 +0900

    landmarks-Building lists and Navigation

---
+++ LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by newgeneration on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by newgeneration on 2022/02/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
