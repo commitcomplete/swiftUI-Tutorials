460e6df3f8bd56eae65a66c380ca958fda26b7a7
commitcomplete <oneblue14@gmail.com>
2022-02-15 20:52:01 +0900

    Handling User Input

---
+++ Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,46 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by newgeneration on 2022/02/14.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    
+    @State private var showFavoritesOnly=false
+    @EnvironmentObject var modelData: ModelData
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+               (!showFavoritesOnly || landmark.isFavorite)
+           }
+       }
+    var body: some View {
+        NavigationView{
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                                   Text("Favorites only")
+                               }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                                } label: {
+                    LandmarkRow(landmark: landmark)
+                                }
+                            }
+        }
+        .navigationTitle("Landmarks")
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+                   LandmarkList()
+                .environmentObject(ModelData())
+               }
+
+    }
+}
+}
